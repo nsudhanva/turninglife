f45c7dda65a5dc2c5506177a5ad4f104a204db3d
Sudhanva <nsudhanva@gmail.com>
2018-04-17 13:05:07 +0530

    Added fonts and images not included

--- vendor/assets/javascripts/main.js
+++ vendor/assets/javascripts/main.js
@@ -311,29 +311,29 @@
 			return false;
 		});
 		
-		// Google maps:
-		if ($('#map-canvas').size() > 0) {
-			function initialize() {
-				var styleArray = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}];
-				var myLatlng = new google.maps.LatLng(40.714353, -74.005973);
-				var mapOptions = {
-					zoom: 15,
-					scrollwheel: false,
-					center: myLatlng,
-					mapTypeId: google.maps.MapTypeId.ROADMAP,
-					styles: styleArray
-				};
-				var map = new google.maps.Map(doc.getElementById('map-canvas'), mapOptions);
-				var marker = new google.maps.Marker({
-					position: myLatlng,
-					map: map,
-					title: 'New York!',
-					icon: 'img/icons/map_icon.png'
-				});
-			}
-			google.maps.event.addDomListener(window, 'load', initialize);
-		};
-		
+		// // Google maps:
+		// if ($('#map-canvas').size() > 0) {
+		// 	function initialize() {
+		// 		var styleArray = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}];
+		// 		var myLatlng = new google.maps.LatLng(40.714353, -74.005973);
+		// 		var mapOptions = {
+		// 			zoom: 15,
+		// 			scrollwheel: false,
+		// 			center: myLatlng,
+		// 			mapTypeId: google.maps.MapTypeId.ROADMAP,
+		// 			styles: styleArray
+		// 		};
+		// 		var map = new google.maps.Map(doc.getElementById('map-canvas'), mapOptions);
+		// 		var marker = new google.maps.Marker({
+		// 			position: myLatlng,
+		// 			map: map,
+		// 			title: 'New York!',
+		// 			icon: 'img/icons/map_icon.png'
+		// 		});
+		// 	}
+		// 	google.maps.event.addDomListener(window, 'load', initialize);
+		// };
+		//
 		// Coming soon:
 		if ($('#countdown').size() > 0) {
 			var	austDay = new Date(2017, 19 - 1, 3);
