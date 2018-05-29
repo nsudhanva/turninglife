955ecfc094d0c47697fd52d9328fa779f16e6340
Sudhanva <nsudhanva@gmail.com>
2018-05-29 10:51:26 +0530

    Added all pages

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,28 @@
 class HomesController < ApplicationController
   def index
   end
+
+  def about
+  end
+
+  def services
+  end
+
+  def counselling
+  end
+
+  def appointment
+  end
+
+  def faq
+  end
+
+  def team
+  end
+
+  def gallery
+  end
+
+  def contacts
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   resources :categories
   resources :therapists
   get 'homes/index'
+  get 'homes/about'
+  get 'homes/services'
+  get 'homes/counselling'
+  get 'homes/faq'
+  get 'homes/team'
+  get 'homes/gallery'
+  get 'homes/contacts'
   root to: "homes#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
