4d64e88cbeaf0bb9dca14518b0aebb858f4b6a02
Sudhanva <nsudhanva@gmail.com>
2018-08-02 12:23:56 +0530

    Changed contents of all erb files

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :treatments
   resources :categories
   resources :therapists
+
   get 'homes/index'
   get 'homes/about'
   get 'homes/services'
@@ -11,6 +12,7 @@ Rails.application.routes.draw do
   get 'homes/team'
   get 'homes/gallery'
   get 'homes/contacts'
+  
   root to: "homes#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
