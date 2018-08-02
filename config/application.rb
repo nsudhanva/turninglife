83ce2f43aa8a0f343115cebaaf80119abdb93b6f
Sudhanva <nsudhanva@gmail.com>
2018-08-02 11:01:12 +0530

    Revert commit back before assets

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Turninglife
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
